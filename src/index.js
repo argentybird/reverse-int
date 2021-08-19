fd11e5c31227b415cd400af827f1a8806b5971b7
Marina Miroslava <argenty@gmail.com>
2021-08-19 17:44:56 +0300

    everything works

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(x) {
+
+    if (x < 0) {
+        x = x * (-1);
+    }
+
+    x = String(x);
+    x = x.split("");
+    x = x.reverse();
+    x = x.join("");
+
+    x = Number(x);
+    return x;
 }
